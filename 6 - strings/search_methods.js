4165d5c89abf00599fa478b7075353ac34dd0b0d
Antonio <toninetocontato@gmail.com>
2023-02-14 17:05:09 -0300

    search and indexOf

--- 6 - strings/search_methods.js
+++ 6 - strings/search_methods.js
@@ -11,3 +11,8 @@ String endsWith()
 
 */
 
+let str = "Please locate where 'locate' occurs!";
+// Segundo parâmetro: serve para definir índice de partida
+console.log(str.indexOf("locate", 15));
+// Diferença entre search e indexOf: search aceita regexp
+console.log(str.search(/locate/));
