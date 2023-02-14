6ff014d6137510bf7f2559c4fa859833a358650a
Antonio <toninetocontato@gmail.com>
2023-02-14 17:10:46 -0300

    string match

--- 6 - strings/search_methods.js
+++ 6 - strings/search_methods.js
@@ -13,6 +13,11 @@ String endsWith()
 
 let str = "Please locate where 'locate' occurs!";
 // Segundo parâmetro: serve para definir índice de partida
-console.log(str.indexOf("locate", 15));
 // Diferença entre search e indexOf: search aceita regexp
+console.log(str.indexOf("locate", 15));
 console.log(str.search(/locate/));
+
+// g (global) = busca tudo 
+// i (insensitive) = desconsidera case sensitive
+let text = "The rain in SPAIN stays mainly in the plain";
+text.match(/ain/gi);
