0cd029f57b8c822017141846b743212860cc151f
Antônio Neto <toninetocontato@gmail.com>
2023-03-02 15:48:19 -0300

    emit method comment

--- extra/event_emitter_node.js
+++ extra/event_emitter_node.js
@@ -9,5 +9,8 @@ eventEmitter.on('printWord', (word) => {
   console.log('A palavra é ' + word)
 })
 
-
+// Já no método emit, o primeiro parâmetro corresponde
+// a string definida para chamar o trigger e o restante
+// são parâmetros que podem ser passados para a função
+// callback
 eventEmitter.emit('printWord', 'Toni')
