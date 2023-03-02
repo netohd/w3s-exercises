16acf5da844895f38fb492fc907787f3fe6d7c25
Antônio Neto <toninetocontato@gmail.com>
2023-03-02 15:47:07 -0300

    on method comment

--- extra/event_emitter_node.js
+++ extra/event_emitter_node.js
@@ -1,8 +1,13 @@
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
 
+// O método on define um trigger para ser acionado
+// definindo a string que o invoca no primeiro parâmetro
+// e no segundo parâmetro é definida uma função callback
+// que pode receber argumentos
 eventEmitter.on('printWord', (word) => {
   console.log('A palavra é ' + word)
 })
+
 
 eventEmitter.emit('printWord', 'Toni')
