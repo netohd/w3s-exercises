2e6cf82dc2e3bb160a6813d4ff6f235cba357a64
Antônio Neto <toninetocontato@gmail.com>
2023-03-10 22:24:58 -0300

    change callback to arrow function

--- extra/callback_functions.js
+++ extra/callback_functions.js
@@ -1,11 +1,12 @@
-function recebeOutraFunction(callbackSoma) {
+function recebeOutraFunction(callbackSoma, ...resto) {
   let a = 10, b = 9
   callbackSoma(a, b)
+  console.log(resto)
 }
 
 // A função callbackSoma é passada como argumento e chamada
 // dentro da outra função que a recebe, já com os valores
-recebeOutraFunction(function callbackSoma(x, y) {
+recebeOutraFunction((x, y) => {
   soma = x + y
   console.log("A soma é " + soma)
-})
+}, 'teste')
