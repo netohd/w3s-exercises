7e41aa5fad84819eb31edb662d66e0e2f4a3ffa8
Antônio Neto <toninetocontato@gmail.com>
2023-04-02 16:40:28 -0300

    for of solution

--- extra/async_await.js
+++ extra/async_await.js
@@ -1,6 +1,3 @@
-// https://api.github.com/users/${username}
-// https://api.github.com/users/${username}/repos
-
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -18,28 +15,33 @@ async function searchUser(user) {
 async function main() {
   const data = await searchUser('netohd')
   console.log(data, 'data here')
-  console.log('teste')
+  console.log('test')
 }
 
 async function forEachTest() {
   const artists = await prisma.artist.findMany()
-  console.log(artists[50], 'artists')
+  console.log(artists[50], 'artist')
   const allElements = []
-
-  artists.forEach(async element => {
-    let currentElement = await prisma.customer.findUnique({
+  allElements.push({ nome: 'Antonio' })
+  
+  // [N]ao funciona] => artists.forEach(async element => {
+  // Explicação: forEach é um loop que não espera chamadas
+  // assíncronas. Solução: for of
+  for (let element of artists ) {
+    let currElement = await prisma.customer.findUnique({
       where: {
         CustomerId: element.ArtistId,
       },
     })
-    allElements.push(currentElement)
-  });
-
-
+    allElements.push(currElement)
+  }
   return allElements
-  
 }
 
-const result = forEachTest()
-console.log(result, 'result here')
+async function main2() {
+  const result = await forEachTest()
+  console.log(result[1], result.length, 'result here')
+}
+
+main2()
 // main()
