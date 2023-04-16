04bae9e3eb47ea6834ffcc62ef47ee46e09a6d29
Antônio Neto <toninetocontato@gmail.com>
2023-04-16 00:02:19 -0300

    objects advanced

--- 17 - objects/objects.js
+++ 17 - objects/objects.js
@@ -1,20 +1,37 @@
-let carro = {
-  marca: String,
-  modelo: String,
-  ano: Number,
-
-  carro: function(marca, modelo, ano) {
-    this.marca = marca
-    this.modelo = modelo
-    this.ano = ano
-  },
-
-  mostraModelo: function() {
-    return this.modelo
+// Criando objeto com a factory (não tem new, diferente da constructor)
+function criaCarro(marca, modelo, ano) {
+  return {
+    marca,
+    modelo,
+    ano,
+    mudaCarro(marca, modelo, ano) {
+      this.marca = marca
+      this.modelo = modelo
+      this.ano = ano
+    },
+    mostraInfo() {
+      return `Este carro é o ${this.modelo} da marca ${this.marca}`
+    }
   }
 }
 
-let gol = new carro('volks', 'gol trend', 1999)
+let gol = criaCarro('volks', 'gol trend', 1999)
+console.log(gol.mostraInfo())
 
-console.log(gol)
+let golLiteral = {
+  marca: 'volks',
+  modelo: 'gol trend',
+  ano: 1999,
+  mudaAno(novoAno) {
+    this.ano = novoAno
+  }
+}
+golLiteral.mudaAno(1995)
+console.log(golLiteral)
 
+// Também é possível acessar as chaves dos objetos com [] ao invés de .
+console.log(gol['marca'])
+
+for (let chave in gol) {
+  console.log(chave)
+}
