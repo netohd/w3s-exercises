332961de5afec34aadda29bd8f4cde9638ad3165
Antônio Neto <toninetocontato@gmail.com>
2023-04-16 21:00:03 -0300

    express testing

--- 50 - express/server.js
+++ 50 - express/server.js
@@ -1,9 +1,32 @@
 const express = require('express')
-
 const app = express()
 
+// Isso "converte" o req.body em um objeto pra não ser undefined
+app.use(express.urlencoded({
+    extended: true
+}))
+
 app.get('/', (req, res) => {
-    res.send('Olá mundo!')
+    res.send(`
+    <div id='minhaDiv'>
+        <form action="/" method="post">
+            <label>Seu CEP</label>
+            <input type="text" name="cep">
+            <button>Enviar</button>
+        </form> 
+    </div>
+    `)
+})
+
+app.get('/testes/:valor?/:segundo?', (req, res) => {
+    console.log(req.params, 'params here')
+    console.log(req.query, 'query here')
+    // res.send(200, req.params)
+    res.send(200, req.query)
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.cep, 'cep here')
 })
 
 app.listen(3000, () => {
