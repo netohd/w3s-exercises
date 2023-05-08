d400c47ac45b55a39bb8b801c55f574315b801cd
Antônio Neto <toninetocontato@gmail.com>
2023-05-07 23:53:31 -0300

    Update server.js

--- 50 - express/server.js
+++ 50 - express/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { router } from './routes.js'
 const app = express()
 
-// Isso "converte" o req.body em um objeto pra não ser undefined
+// "Converte" o req.body em um objeto pra não ser undefined
 app.use(express.urlencoded({
     extended: true
 }))
